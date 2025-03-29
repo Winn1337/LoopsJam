ead8dda738e9937616150bfade06bef792e52122
Winn1337 <112430462+Winn1337@users.noreply.github.com>
2025-03-29 19:36:53 +0100

    Skybox and timer

--- Assets/Restarter.cs
+++ Assets/Restarter.cs
@@ -1,17 +1,37 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Restarter : MonoBehaviour
 {
+    public TextMeshProUGUI timerText;
+    static float timer;
+
+    private void Awake()
+    {
+        timer = PlayerPrefs.GetFloat("timer", 0);
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.R) || Input.GetKeyDown(KeyCode.Space))
             Restart();
+
+        timer += Time.unscaledDeltaTime;
+
+        TimeSpan t = TimeSpan.FromSeconds(timer);
+        timerText.text = "Time wasted: " + t.ToString("hh") + '.' + t.ToString("mm") + '.' + t.ToString("ss");
     }
 
     public static void Restart()
     {
+        PlayerPrefs.SetFloat("timer", timer);
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void OnApplicationQuit()
+    {
+        PlayerPrefs.Save();
     }
 }
