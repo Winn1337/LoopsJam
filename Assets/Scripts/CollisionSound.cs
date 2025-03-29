d6a23700cbc2211b487ec30a3e4feec827f9b904
Winn1337 <112430462+Winn1337@users.noreply.github.com>
2025-03-29 19:57:35 +0100

    Impact sounds

---
+++ Assets/Scripts/CollisionSound.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+using UnityEngine.UI;
+
+public class CollisionSound : MonoBehaviour
+{
+    public AudioClip metalClip;
+    public LayerMask metalLayer;
+
+    public AudioClip[] thuds;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if ((metalLayer & (1 << collision.gameObject.layer)) != 0)
+        {
+            AudioSource.PlayClipAtPoint(metalClip, collision.contacts[0].point);
+        }
+
+        AudioSource.PlayClipAtPoint(thuds[Random.Range(0, thuds.Length)], collision.contacts[0].point);
+    }
+}
--- README.md
+++ README.md
@@ -8,3 +8,11 @@ The game must use only two buttons for controls
 The game must have multiple camera perspectives (first person, fixed, isometric etc.)
 ### Difficult
 Weather or Environmental Effects that affect the game
+
+# Credits
+## Great idea
+Oscar B
+
+## Sounds
+https://freesound.org/people/jorickhoofd/sounds/160045/
+https://freesound.org/people/RICHERlandTV/sounds/232358/
