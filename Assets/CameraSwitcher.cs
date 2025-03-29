2ff1d86dc5ec9798fe95cf2347e565501b442a8a
Winn1337 <112430462+Winn1337@users.noreply.github.com>
2025-03-29 01:24:23 +0100

    visuals

--- Assets/CameraSwitcher.cs
+++ Assets/CameraSwitcher.cs
@@ -1,26 +1,41 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 
 public class CameraSwitcher : MonoBehaviour
 {
-    public RenderTexture renderTexture;
-    public Camera[] cameras;
-    int active;
-
-    GameObject[] gameObjects;
+    public Camera[] cams;
     AudioListener[] audioListeners;
 
-    void Awake()
-    {
-        gameObjects = new GameObject[cameras.Length];
-        audioListeners = new AudioListener[cameras.Length];
+    public bool toggle;
+    public RenderTexture renderTexture;
+    int active;
 
-        for (int i = 0; i < cameras.Length; i++)
+#if UNITY_EDITOR
+    void OnValidate()
+    {
+        if (cams == null || cams.Length == 0)
+            return;
+
+        if (toggle)
         {
-            gameObjects[i] = cameras[i].gameObject;
-            audioListeners[i] = cameras[i].GetComponent<AudioListener>();
+            DoToggle();
+            toggle = false;
+            return;
+        }
+
+        audioListeners = new AudioListener[cams.Length];
+        for (int i = 0; i < cams.Length; i++)
+        {
+            if (!cams[i])
+                continue;
+
+            audioListeners[i] = cams[i].GetComponent<AudioListener>();
         }
     }
+#endif
 
     void Update()
     {
@@ -28,16 +43,17 @@ public class CameraSwitcher : MonoBehaviour
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 
         if (Input.GetKeyDown(KeyCode.C))
-        {
-            //gameObjects[active].SetActive(false);
-            cameras[active].targetTexture = renderTexture;
-            audioListeners[active].enabled = false;
+            DoToggle();
+    }
 
-            active = (active + 1) % cameras.Length;
+    void DoToggle()
+    {
+        cams[active].targetTexture = renderTexture;
+        audioListeners[active].enabled = false;
 
-            //cameras[active].gameObject.SetActive(true);
-            cameras[active].targetTexture = null;
-            audioListeners[active].enabled = true;
-        }
+        active = (active + 1) % cams.Length;
+
+        cams[active].targetTexture = null;
+        audioListeners[active].enabled = true;
     }
 }
