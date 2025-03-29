0157c57855b798d45237ace984a0468a0c770915
Johannes Widén <johanneswiden2003@gmail.com>
2025-03-29 15:42:50 +0100

    Bar jumping

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,7 +1,10 @@
+using System.Net;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Transform upperBody;
     public HingeJoint armJoint;
     public float upVel = 250f;
     public float upForce = 250f;
@@ -9,6 +12,14 @@ public class PlayerMovement : MonoBehaviour
     public float downForce = -100f;
 
     public HingeJoint[] legJoints;
+    public Transform handPos;
+    public float handRadius;
+    public LayerMask barMask;
+
+    public Vector3 armAnchor;
+    public Vector3 armAxis;
+
+    Transform lastBar;
 
     void Awake()
     {
@@ -26,6 +37,31 @@ public class PlayerMovement : MonoBehaviour
             motor.targetVelocity = up ? upVel : downVel;
             motor.force = up ? upForce : downForce;
             joint.motor = motor;
+        }
+
+        if (!armJoint)
+        {
+            Collider[] bars = Physics.OverlapSphere(handPos.position, handRadius, barMask);
+
+            if (bars.Length > 0)
+            {
+                if (bars[0].transform == lastBar)
+                    return;
+
+                Vector3 relPos = bars[0].transform.position - handPos.position;
+                upperBody.position += relPos;
+                upperBody.eulerAngles = new Vector3(0, 0, upperBody.eulerAngles.z);
+
+                armJoint = upperBody.AddComponent<HingeJoint>();
+                armJoint.anchor = armAnchor;
+                armJoint.axis = armAxis;
+
+                lastBar = bars[0].transform;
+            }
+            else
+            {
+                lastBar = null;
+            }
         }
 
         if (Input.GetKeyDown(KeyCode.X) && armJoint)
