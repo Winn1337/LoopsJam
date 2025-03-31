be12e8b174017dd5e6b2f4781d2184e5f3ee2e07
Winn1337 <112430462+Winn1337@users.noreply.github.com>
2025-03-31 22:40:57 +0200

    Improve sound fx

--- Assets/Scripts/CollisionSound.cs
+++ Assets/Scripts/CollisionSound.cs
@@ -9,11 +9,14 @@ public class CollisionSound : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        float relvel = Mathf.Abs(Vector3.Dot(collision.relativeVelocity, collision.contacts[0].normal));
+        float volume = Mathf.InverseLerp(0, 8, relvel);
+
         if ((metalLayer & (1 << collision.gameObject.layer)) != 0)
         {
-            AudioSource.PlayClipAtPoint(metalClip, collision.contacts[0].point);
+            AudioSource.PlayClipAtPoint(metalClip, collision.contacts[0].point, volume);
         }
 
-        AudioSource.PlayClipAtPoint(thuds[Random.Range(0, thuds.Length)], collision.contacts[0].point);
+        AudioSource.PlayClipAtPoint(thuds[Random.Range(0, thuds.Length)], collision.contacts[0].point, volume * 1.5f);
     }
 }
